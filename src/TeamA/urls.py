c0fdfe9b29b0733af6eb5982e0de9c22b96be30f
i-hori <16beat.takeshi@gmail.com>
2023-11-06 15:16:45 +0900

    Revert "Merge remote-tracking branch 'origin/back-front-marge-test1' into front-test"
    This reverts commit 4737e9b07d1cd69ddf88b9dda79ba2fda0c749c2, reversing
    changes made to 9c9a7eefbf07401fb0ec75a7a6fed416829b7447.

--- src/TeamA/urls.py
+++ src/TeamA/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('accounts/', include('allauth.urls')),
 ]
 
 # 画像のURLを追加
