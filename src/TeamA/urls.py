0b2983bedb16aa2a334778927d2f636d926dfa70
ohr72405 <ohr2272405@gmail.com>
2023-11-13 09:57:55 +0900

    トリアナ様レビュー用

--- src/TeamA/urls.py
+++ src/TeamA/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('store/', include('store.urls')),
 ]
 
 # 画像のURLを追加
