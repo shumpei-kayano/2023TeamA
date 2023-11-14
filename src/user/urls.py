442baec25e36db0da48401bdd990de3b83fd0dcf
Kiyota-Riku <rikukou1124528@gmail.com>
2023-11-14 12:31:43 +0900

    ログインページ作成

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login_view
 
 app_name = 'user'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
 ]
