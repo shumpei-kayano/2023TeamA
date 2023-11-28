fe44ed5ee2f4d66bd1140e44aeaecac43a2c6aeb
anaxxx777 <axnxaxyxo@icloud.com>
2023-11-28 15:44:46 +0900

    実際のページから登録できた

--- src/store/forms.py
+++ src/store/forms.py
@@ -2,18 +2,6 @@ from django import forms
 from .models import Product, Sale
 
 class ProductForm(forms.ModelForm):
-    # product_name = forms.CharField(
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'input-active',
-    #         'placeholder': '商品名を入力してください',
-    #         'required': 'required'
-    #     })
-    # )
-    # product_category = forms.ChoiceField(
-    #     choices=Product.TASTE_CHOICES,
-    #     widget=forms.Select(attrs={'class': 'input-active'})
-    # )
-
     class Meta:
         model = Product
         exclude = ['store'] # storeは自動で入るので、フォームには表示しない
@@ -42,18 +30,11 @@ class ProductForm(forms.ModelForm):
         }
 
 class SaleForm(forms.ModelForm):
-    # sale_price = forms.DecimalField(
-    #     widget=forms.NumberInput(attrs={
-    #         'class': 'input-active',
-    #         'placeholder': '販売価格を入力してください（※半角数字）',
-    #         'required': 'required'
-    #     })
-    # )
     class Meta:
         model = Sale
         exclude = ['product', 'store']
         fields = ['sale_price', 'sale_start', 'sale_end', 'stock', 'expiration_date', 'description', 'sale_type']
-        widget = {
+        widgets = {
             'sale_price': forms.NumberInput(attrs={
                 'class': 'input-active', 
                 'placeholder': '販売価格を入力してください（※半角数字）', 
