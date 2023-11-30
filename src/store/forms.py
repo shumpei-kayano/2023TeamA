4ba952a9ad52429f3b0c7a0469b70d392fa2a6be
anaxxx777 <axnxaxyxo@icloud.com>
2023-11-30 15:22:37 +0900

    商品登録後、モーダル動く修正ok

--- src/store/forms.py
+++ src/store/forms.py
@@ -21,6 +21,7 @@ class ProductForm(forms.ModelForm):
                 'required': 'required'
             }),
             'product_image': forms.ClearableFileInput(attrs={
+                'id': 'example',
                 'class': 'input-active', 
                 'placeholder': '画像を選択してください',
                 'required': 'required',
@@ -62,12 +63,14 @@ class SaleForm(forms.ModelForm):
                 'required': 'required'
             }),
             'sale_start': forms.DateTimeInput(attrs={
+                'id': 'startDateTime',
                 'class': 'input-active', 
                 'title': '販売開始日時を入力してください',
                 'required': 'required',
                 'type': 'datetime-local'
             }),
             'sale_end': forms.DateTimeInput(attrs={
+                'id': 'endDateTime',
                 'class': 'input-active', 
                 'title': '販売終了日時を入力してください',
                 'required': 'required',
