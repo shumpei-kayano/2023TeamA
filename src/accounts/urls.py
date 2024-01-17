272879e73e8da57ff258f95972ce0105c00d7aeb
i-hori <16beat.takeshi@gmail.com>
2024-01-17 13:50:36 +0900

    お客様情報変更完成

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'), #追加
     path('reset/done/', views.PasswordResetComplete.as_view(), name='password_reset_complete'), #追加
     path('mypage/', views.MypageView, name='mypage'),
+    path('re_cust/', views.Re_custView, name='re_cust'),
 ]
