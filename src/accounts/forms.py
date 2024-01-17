fd4135500e75ceb2c0eef2c9ad9b5c894d3379e8
anaxxx777 <axnxaxyxo@icloud.com>
2024-01-17 10:29:56 +0900

    - 店舗新規登録画面修正中

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -51,13 +51,63 @@ class MelimitUserLoginForm(forms.Form):
         model = MelimitUser
         fields = ['email', 'password']
 
-# MelimitStore用会員情報登録＆編集フォーム
+# 全都道府県をリスト化(copilot依頼)
+PREFECTURE_CHOICES = [('','選択してください'),('北海道','北海道'),('青森県','青森県'),('岩手県','岩手県'),('宮城県','宮城県'),('秋田県','秋田県'),('山形県','山形県'),('福島県','福島県'),('茨城県','茨城県'),('栃木県','栃木県'),('群馬県','群馬県'),('埼玉県','埼玉県'),('千葉県','千葉県'),('東京都','東京都'),('神奈川県','神奈川県'),('新潟県','新潟県'),('富山県','富山県'),('石川県','石川県'),('福井県','福井県'),('山梨県','山梨県'),('長野県','長野県'),('岐阜県','岐阜県'),('静岡県','静岡県'),('愛知県','愛知県'),('三重県','三重県'),('滋賀県','滋賀県'),('京都府','京都府'),('大阪府','大阪府'),('兵庫県','兵庫県'),('奈良県','奈良県'),('和歌山県','和歌山県'),('鳥取県','鳥取県'),('島根県','島根県'),('岡山県','岡山県'),('広島県','広島県'),('山口県','山口県'),('徳島県','徳島県'),('香川県','香川県'),('愛媛県','愛媛県'),('高知県','高知県'),('福岡県','福岡県'),('佐賀県','佐賀県'),('長崎県','長崎県'),('熊本県','熊本県'),('大分県','大分県'),('宮崎県','宮崎県'),('鹿児島県','鹿児島県'),('沖縄県','沖縄県')]
+# MelimitStore用会員情報登録＆編集フォーム 
 class MelimitStoreRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
     class Meta:
         model = MelimitStore
         fields = ['email', 'password', 'password_confirm', 'username', 'postal_code', 'prefecture', 'city', 'address', 'phone_number', 'store_image', 'site_url']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'store-create-input-active',
+                'placeholder': 'メールアドレスを入力してください（半角）',
+                'required': True,
+                'pattern': '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'store-create-input-active',
+                'pattern': '^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,20}$',
+                'placeholder': 'パスワードを入力してください（半角）'
+            }),
+            'password_confirm': forms.PasswordInput(attrs={
+                'class': 'store-create-input-active',
+                'pattern': '^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,20}$',
+                'placeholder': 'パスワードを再入力してください（半角）'
+            }),
+            'username': forms.TextInput(attrs={
+                'class': 'store-create-input-active',
+                'pattern': '^.{1,20}$',
+                'placeholder': '店舗名を入力してください'
+            }),
+            'postal_code': forms.TextInput(attrs={
+                'class': 'store-create-input-active',
+                'pattern': '^[0-9]{7}$',
+                'placeholder': '郵便番号（半角数字）を入力してください（ハイフン不要）'}),
+                'prefecture': forms.Select(choices=PREFECTURE_CHOICES, attrs={
+                'class': 'store-create-input-active'
+            }),
+            'city': forms.TextInput(attrs={
+                'class': 'store-create-input-active'
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'store-create-input-active'
+                }),
+            'phone_number': forms.TextInput(attrs={
+                'class': 'store-create-input-active',
+                'pattern': '^[0-9]{10,11}$',
+                'placeholder': '電話番号（半角数字）を入力してください（ハイフン不要）'
+            }),
+            'store_image': forms.FileInput(attrs={
+                'class': 'store-create-input-active'
+            }),
+            'site_url': forms.URLInput(attrs={
+                'class': 'store-create-input-active',
+                'placeholder': 'WebサイトのURLがある場合は入力してください（半角）'
+            }),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
